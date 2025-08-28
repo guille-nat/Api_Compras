0a3866b5ad3da4fe1d3adf702e619757a4e4c72a
Guillermo Natali Ulla <22guillermonatali@gmail.com>
2025-08-28 09:27:05 -0300

    feat: se crearon los views públicos y privados

--- API_Compras/api/categories/views.py
+++ API_Compras/api/categories/views.py
@@ -1,23 +1,35 @@
 from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-from .serializer import CategorySerializer
-from .models import Category
+from .serializer import CategoryPrivateSerializer, CategoryPublicSerializer
+from . import services, selectors
 
 
 class CategoryPrivateViewSet(ModelViewSet):
-    queryset = Category.objects.all().order_by('name')
-    serializer_class = CategorySerializer
+    serializer_class = CategoryPrivateSerializer
     permission_classes = [IsAdminUser]
 
+    def get_queryset(self):
+        return selectors.list_categories_admin()
+
     def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user,
-                        updated_by=self.request.user)
+        category = services.create_category(
+            user=self.request.user, name=serializer.validated_data["name"])
+        # reasigna la instancia de la categoría
+        serializer.instance = category
 
     def perform_update(self, serializer):
-        serializer.save(updated_by=self.request.user)
+        category = self.get_object()
+
+        # Solo se aplica si se puede renombrar.
+        if "name" in serializer.validated_data:
+            services.rename_category(
+                user=self.request.user, category=category, new_name=serializer.validated_data["name"])
+            serializer.instance.refresh_from_db()
 
 
 class CategoryPublicViewSet(ReadOnlyModelViewSet):
-    queryset = Category.objects.all().order_by('name')
-    serializer_class = CategorySerializer
+    serializer_class = CategoryPublicSerializer
     permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return selectors.list_categories_public()
