6fa56f4b79184210ff4564f98ccda795ffac9b3e
Guillermo Natali Ulla <22guillermonatali@gmail.com>
2025-08-28 09:25:32 -0300

    feat: creación de los serializer público y privado con validación a la hora de crear una categoría

--- API_Compras/api/categories/serializer.py
+++ API_Compras/api/categories/serializer.py
@@ -1,15 +1,25 @@
-from rest_framework.serializers import ModelSerializer
+# serializers.py
+from rest_framework import serializers
 from .models import Category
 
 
-class CategorySerializer(ModelSerializer):
+class CategoryPrivateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
-        read_only_fields = [
-            'id',
-            'created_by',
-            'created_at',
-            'updated_by',
-            'updated_at'
+        fields = [
+            "id", "name",
+            "created_by", "created_at", "updated_by", "updated_at",
         ]
+        read_only_fields = ["id", "created_by",
+                            "created_at", "updated_by", "updated_at"]
+
+    def validate_name(self, value):
+        if len(value.strip()) < 2:
+            raise serializers.ValidationError("El nombre es demasiado corto.")
+        return value.strip()
+
+
+class CategoryPublicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
